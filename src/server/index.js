e1e2a785fa0d40e3ce79765ea1d2f05f8b4c7ada
William Robertson <williamrobertson.13@gmail.com>
2018-02-06 13:33:08 -0500

    Setup ESLint following Airbnb's style guide

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const app = express();
 
 const publicPath = express.static('./dist');
@@ -11,12 +9,12 @@ app.get('/', (req, res) => {
   res.sendFile('index.html', { root: '.' });
 });
 
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   res.status(err.status || 500);
   res.json({
-  	error: {
-  	  message: err.message
-  	}
+    error: {
+      message: err.message,
+    },
   });
 });
 
