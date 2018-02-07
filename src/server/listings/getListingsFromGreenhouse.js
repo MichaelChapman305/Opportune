8290b67376505ca64838815d8754766779ddf02a
Michael Chapman <chapmanm305@gmail.com>
2018-02-06 19:32:49 -0500

    Creates fetch for job listing from Greenhouse

--- src/server/listings/getListingsFromGreenhouse.js
+++ src/server/listings/getListingsFromGreenhouse.js
@@ -3,16 +3,13 @@ const fetch = require('node-fetch');
 function getListingsFromGreenhouse(companyName) {
   fetch(`https://api.greenhouse.io/v1/boards/${companyName}/jobs`)
     .then(res => res.json())
-    .then(json => {
-      const jobs = json.jobs.map(job => {
-        return {
-          id: job.id,
-          title: job.title,
-          location: job.location && job.location.name,
-          url: job.absolute_url,
-        };
-      });
-      console.log(jobs);
+    .then((json) => {
+      const jobs = json.jobs.map(job => ({
+        id: job.id,
+        title: job.title,
+        location: job.location && job.location.name,
+        url: job.absolute_url,
+      }));
 
       return jobs;
     }).catch(err => console.error('Error fetching greenhouse data', err));
