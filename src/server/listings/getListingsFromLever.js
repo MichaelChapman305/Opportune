85fad17e605f064495734bd9b84819d12f6b1ba4
William Robertson <williamrobertson.13@gmail.com>
2018-02-06 19:05:53 -0500

    Configure lint rule

--- src/server/listings/getListingsFromLever.js
+++ src/server/listings/getListingsFromLever.js
@@ -4,17 +4,15 @@ function getListingsFromLever(companyName) {
   fetch(`https://api.lever.co/v0/postings/${companyName}?&mode=json`)
     .then(res => res.json())
     .then(json => {
-      const jobs = json.map(job => {
-        return {
-          id: job.id,
-          title: job.text,
-          location: job.categories && job.categories.location,
-          url: job.hostedUrl,
-        };
-      });
+      const jobs = json.map(job => ({
+        id: job.id,
+        title: job.text,
+        location: job.categories && job.categories.location,
+        url: job.hostedUrl,
+      }));
 
       return {
-        [companyName]: jobs
+        [companyName]: jobs,
       };
     }).catch(err => console.error('Error fetching lever data', err));
 }
